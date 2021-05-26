a7e6e645c0bbc38c721054266f7ac70752492c40
Hyun-je <bigae2@gmail.com>
2021-05-26 16:59:46 +0900

    Update docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -2,4 +2,4 @@
 xhost +local:docker
 
 # run docker container
-docker run -it --privileged --env=DISPLAY --network=host -v /dev:/dev -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/rosbag:/root/rosbag ros:velodyne-record
+docker run -it --privileged --env=DISPLAY --network=host -v /dev:/dev -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/rosbag:/root/rosbag velodyne_wt61c
